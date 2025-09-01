58dbeb6c589285c3c9418d1077633ac041cd3966
DrSabya <147489508+DrSabya@users.noreply.github.com>
2025-09-01 21:16:15 +0530

    private updated

--- app/private/page.tsx
+++ app/private/page.tsx
@@ -1,14 +1,14 @@
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 
-import { createClient } from '@/utils/supabase/server'
+// import { createClient } from '@/utils/supabase/server'
 
-export default async function PrivatePage() {
-  const supabase = await createClient()
+// export default async function PrivatePage() {
+//   const supabase = await createClient()
 
-  const { data, error } = await supabase.auth.getUser()
-  if (error || !data?.user) {
-    redirect('/auth/login')
-  }
+//   const { data, error } = await supabase.auth.getUser()
+//   if (error || !data?.user) {
+//     redirect('/auth/login')
+//   }
 
-  return <p>Hello {data.user.email}</p>
-}
+//   return <p>Hello {data.user.email}</p>
+// }
